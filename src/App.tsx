b325e80923fd118d01505f5c35fcc85bab1b0c4b
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-06-01 01:08:05 +0800

    refactor: move preselected currency to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from "react-router-dom";
-import CurrencyProvider from "@/context/currency";
+import { useEffect } from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
+import CurrencyProvider, { currencies, useCurrencyContext } from "@/context/currency";
 import CartProvider from "@/context/cart";
+import { ActionTypes } from "@/context/currency/currencyReducer";
 import { Layout } from "@/components/shared/Layout";
 import { ShoppingCart } from "@/components/feature/ShoppingCart";
 
 const App = () => {
+    const [searchParams] = useSearchParams();
+    const [, dispatch] = useCurrencyContext();
+
+    useEffect(() => {
+        const currencyId = searchParams.get("currency");
+
+        if (currencyId) {
+            const currency = currencies.find((currency) => currency.id === currencyId);
+
+            if (currency) {
+                dispatch({
+                    type: ActionTypes.SET_CURRENCY,
+                    payload: currency,
+                });
+            }
+        }
+    });
+
     return (
         <CurrencyProvider>
             <CartProvider>
--- src/context/currency/index.tsx
+++ src/context/currency/index.tsx
@@ -1,6 +1,5 @@
-import { createContext, Dispatch, FC, ReactNode, useContext, useEffect, useReducer } from "react";
-import { Action, ActionTypes, currencyReducer } from "@/context/currency/currencyReducer";
-import { useSearchParams } from "react-router-dom";
+import { createContext, Dispatch, FC, ReactNode, useContext, useReducer } from "react";
+import { Action, currencyReducer } from "@/context/currency/currencyReducer";
 
 export type CurrencyState = {
     id: string;
@@ -40,22 +39,6 @@ type CurrencyProviderProps = {
 
 const CurrencyProvider: FC<CurrencyProviderProps> = ({ children }) => {
     const [state, dispatch] = useReducer(currencyReducer, currencies[0]);
-    const [searchParams] = useSearchParams();
-
-    useEffect(() => {
-        const currencyId = searchParams.get("currency");
-
-        if (currencyId) {
-            const currency = currencies.find((currency) => currency.id === currencyId);
-
-            if (currency) {
-                dispatch({
-                    type: ActionTypes.SET_CURRENCY,
-                    payload: currency,
-                });
-            }
-        }
-    });
 
     return (
         <CurrencyContext.Provider value={[state, dispatch]}>{children}</CurrencyContext.Provider>
